97fa16515fa54d9a3e5a8d74068a2487661217ee
lexingxie <lexingxie@gmail.com>
2018-08-26 21:33:33 +1000

    update download instructions

--- code/models/download.sh
+++ code/models/download.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 wget http://users.cecs.anu.edu.au/~u4534172/data/img_to_txt_state.tar 
 wget http://users.cecs.anu.edu.au/~u4534172/data/seq_to_txt_state.tar
+
+# if you are in a system with no wget, e.g. MacOS 
+# then try 
+# curl --remote-name http://users.cecs.anu.edu.au/~u4534172/data/img_to_txt_state.tar (147M) 
+# curl --remote-name http://users.cecs.anu.edu.au/~u4534172/data/seq_to_txt_state.tar (445M)
+# 
+# if you are on windows, then pasting the URL to your browser and save it in this folder! 
