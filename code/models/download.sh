efa829c3f2ced517a54d5c726c4e3e98313e4963
amathews <almath123@gmail.com>
2019-01-02 11:13:44 +1100

    fix broken download links

--- code/models/download.sh
+++ code/models/download.sh
@@ -1,10 +1,4 @@
 #! /bin/sh
-wget http://users.cecs.anu.edu.au/~u4534172/data/img_to_txt_state.tar 
-wget http://users.cecs.anu.edu.au/~u4534172/data/seq_to_txt_state.tar
-
-# if you are in a system with no wget, e.g. MacOS 
-# then try 
-# curl --remote-name http://users.cecs.anu.edu.au/~u4534172/data/img_to_txt_state.tar (147M) 
-# curl --remote-name http://users.cecs.anu.edu.au/~u4534172/data/seq_to_txt_state.tar (445M)
-# 
-# if you are on windows, then pasting the URL to your browser and save it in this folder! 
+echo "Please point your browser to these two links and then download the files each one links too. Place the downloaded files in this directory."
+echo "https://cecs.anu.edu.au/nextcloud/s/AkKWSN54jLc8PnQ"
+echo "https://cecs.anu.edu.au/nextcloud/s/tNJwt3ZMyAaCYG7"
